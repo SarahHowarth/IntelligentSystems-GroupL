44a18127cfab50c702b77243dab69584bd7d9c0a
Sarah Howarth <sarah.a.howarth1@gmail.com>
2020-10-01 12:26:04 +1000

    Setup initial world controller

--- Assets/Scripts/WorldController.cs
+++ Assets/Scripts/WorldController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class WorldController : MonoBehaviour
 {
@@ -8,20 +9,89 @@ public class WorldController : MonoBehaviour
     private static WorldController instance = null;
     public static WorldController Instance => instance;
 
+    [SerializeField] private NavMeshSurface navMesh = default;
+   
+    //ai private data
+    [SerializeField]private GameObject depot;
+    private List<GameObject> dropPoints;
+    private List<GameObject> deliveryVehicles;
 
-
+    //data from UI
+    private int packagesToDeliver = 0; //equates to the number of drop points? 1 package per place 
+    private int numberOfDeliveryAgents = 0;
+    
     // Start is called before the first frame update
     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
     {
         if (instance != null && instance != this)
             Destroy(this);
 
-        instance = this;
+        instance = this; 
     }
+    
+
+    /// <summary>
+    /// UI to call to start simulation
+    /// returns true or false if can start 
+    /// </summary>
+    /// <returns></returns>
+    public bool TryToStart()
+    {
+        if (packagesToDeliver == 0 || numberOfDeliveryAgents == 0) 
+        {
+            return false;
+        }
+        StartSimulation();
+        return true;
+    }
+
+    private void StartSimulation() 
+    {
+        //To Do intialise master routing agent with the private ai data so they can start 
+    
+    }
+
+    private void InstantiateVehicles(int numberOfVehicles) 
+    { 
+        //TO DO Instantiate prefabs once added
+    }
+
+    private void InstantiateDropPoints(int numberOfPackages) 
+    { 
+        //TO DO instantiate prefabs once added 
+    }
+
+
+    //properties
+    public GameObject GetDepot() { return depot; }
+
+    public List<GameObject> GetDeliveryVehicles() { return deliveryVehicles; }
+
+    public List<GameObject> GetDropPoints() { return dropPoints; }
+
+    //ui manager to call to set the packages and vehicles from ui for initialisation
+    public void SetPackagesToDeliver(int packages) 
+    { 
+        packagesToDeliver = packages;
+        InstantiateDropPoints(packagesToDeliver);
+    }
+
+    public void SetNumberOfDeliveryVehicles(int agents) 
+    {
+        numberOfDeliveryAgents = agents;
+        InstantiateVehicles(numberOfDeliveryAgents);
+    }
+
+
+
+
+
+    //extension functions for dynamic addition of packages etc.
+    //will need to link to functions that update the simulation
+    public void AddDropPoint() { }
+    public void AddPackage() { }
+
+    public void RebakeNavMesh() { }
+
+
 }
