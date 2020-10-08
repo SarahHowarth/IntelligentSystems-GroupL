dfb5d51d97f8ca2a7ae73ed81fb655f262c03418
Harry <Harry_Warner1@outlook.com>
2020-10-08 12:08:00 +1100

    added DropPoint and Package classes

---
+++ Assets/Scripts/Package.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Package : MonoBehaviour
+{
+    private int id;
+    private int weight;
+    private DropPoint destination;
+
+    public Package(int id, int weight)
+    {
+
+    }
+
+
+    public DropPoint Destination
+    {
+        get
+        {
+            return destination;
+        }
+        set
+        {
+            destination = value;
+        }
+    }
+
+    public int getID
+    {
+        get
+        {
+            return id;
+        }
+    }
+
+    public int getWeight
+    {
+        get
+        {
+            return weight;
+        }
+    }
+}
