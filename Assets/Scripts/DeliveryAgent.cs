e9e5386f6c300da5b8dce9da15cae455984bb98b
Lachlan Skinner <lach.e98@gmail.com>
2020-10-08 11:33:41 +1100

    current progress, Vehicle enum done

---
+++ Assets/Scripts/DeliveryAgent.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+public class DeliveryAgent : MonoBehaviour
+{
+    private int agentID;
+    private World world;
+    private list<DropPoint> route;
+    private list<Package> packages;
+    private VehicleType type;
+    private Depot depot;
+    private transform currentLoc;
+
+
+    DeliveryAgent(World aWorld, VehicleType aType, Depot aDepot)
+    {
+        world = aWorld;
+        type = aType;
+
+        depot = aDepot;
+        currentLoc = depot.GetPosition()
+    }
+
+    public void SendConstraints()
+    {
+        ACLMessage message;
+
+
+
+        MasterRoutingAgent.ReceiveConstraints(message);
+    }
+
+    public void RequestRoute(ACLMessage message)
+    {
+
+    }
+
+    public bool RecieveRoute(ACLMessage message)
+    {
+
+    }
+
+    public bool DeliverPackages(Transform Location)
+    {
+        bool result == false;
+        foreach i in packages{
+            if(i.GetDestination().GetPosition() == Location)
+            {
+                result = true;
+            }
+        }
+
+        return result;
+    }
+
+    public bool MoveToNextLocation()
+    {
+
+    }
+
+
+    public bool GetLocation()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/VehicleType.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//actual vals assigned to be used as vehicle capacity
+public enum VehicleType : MonoBehaviour
+{
+    truck = 20,
+    van = 15,
+    ute = 10,
+    sedan = 5
+}
