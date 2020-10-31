6c7d9ef7e227d343cbc81e699e11f690a55fa8aa
Sarah Howarth <sarah.a.howarth1@gmail.com>
2020-10-31 11:33:57 +1100

    Update vehicle weight

--- Assets/Scripts/VehicleType.cs
+++ Assets/Scripts/VehicleType.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 //actual vals assigned to be used as vehicle capacity
 public enum VehicleType
 {
-    truck = 12,
-    van = 8,
-    ute = 5,
-    sedan = 2
+    truck = 20,
+    van = 15,
+    ute = 10,
+    sedan = 5
 }
